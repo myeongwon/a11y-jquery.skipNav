e7dbae9ffbfeabc8a54cdb22226ac78e98954496
Jee Hoon <yamoo9@naver.com>
2015-08-17 17:52:35 +0900

    CSS 맵 코드 정리

--- src/jquery.skipNav.js
+++ src/jquery.skipNav.js
@@ -22,35 +22,41 @@
 		// 스킵 내비게이션 요소의 링크에 클래스 설정
 		var $skip_links = $skip.find('a').toggleClass(options.linkClasses, options.customClass);
 
+		// 시작: options.customClass 값이 거짓(false)이면 아래 코드 실행 -------
 		var a11y_hidden_class = {
 			'overflow' : 'hidden',
 			'position' : 'absolute',
-			'clip'     :     'rect(0 0 0 0)',
-			'clip'     :     'rect(0,0,0,0)',
-			'width'    :    '1px',
-			'height'   :   '1px',
-			'margin'   :   -'1px',
-			'padding'  :  '0',
-			'border'   :   '0',
+			'clip'     : 'rect(0 0 0 0)',
+			'clip'     : 'rect(0,0,0,0)',
+			'width'    : '1px',
+			'height'   : '1px',
+			'margin'   : '-1px',
+			'padding'  : 0,
+			'border'   : 0,
 		};
 
 		var a11y_hidden_class_focusable = {
 			'overflow' : 'hidden',
 			'position' : 'absolute',
-			'clip'     :     'rect(0 0 0 0)',
-			'clip'     :     'rect(0,0,0,0)',
-			'width'    :    '1px',
-			'height'   :   '1px',
-			'margin'   :   -'1px',
-			'padding'  :  '0',
-			'border'   :   '0',
+			'clip'     : 'rect(0 0 0 0)',
+			'clip'     : 'rect(0,0,0,0)',
+			'width'    : '1px',
+			'height'   : '1px',
+			'margin'   : '-1px',
+			'padding'  : 0,
+			'border'   : 0,
 		};
 
-		options.customClass || $skip_links.css(a11y_hidden_class).on('focus', function(ev) {
-			$(this).css(a11y_hidden_class_focusable);
-		}).on('blur', function(ev) {
-			$(this).css(a11y_hidden_class);
-		})
+		options.customClass ||
+			$skip_links.css(a11y_hidden_class)
+			.on('focus', function(ev) {
+				$(this).css(a11y_hidden_class_focusable);
+			}).on('blur', function(ev) {
+				$(this).css(a11y_hidden_class);
+			});
+
+		// 끝: options.customClass 값이 거짓(false)이면 아래 코드 실행 -------
+
 
 		// 스킵 내비게이션 요소의 링크 클릭 이벤트 설정
 		$skip.on('click', 'a', function(ev) {
